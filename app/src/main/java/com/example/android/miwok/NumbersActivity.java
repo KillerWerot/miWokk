67f8c44ff3e8a29f8f35016cdcccef8762b1de31
LudoProductions <ludo.productionsmx@gmail.com>
2017-11-14 13:57:57 -0600

    after add recicling views

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,7 +2,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -28,13 +30,12 @@ public class NumbersActivity extends AppCompatActivity {
 
         LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
 
-        for (byte index=0; index < numbers.size(); index++){
-            TextView wordView = new TextView(this);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, numbers);
 
-            wordView.setText(numbers.get(index));
+        ListView listView = (ListView) findViewById(R.id.list);
 
-            rootView.addView(wordView);
+        listView.setAdapter(itemsAdapter);
         }
 
     }
-}
+
