b32ecb30058a51cb15ee59e3fbbd281dc776803b
LudoProductions <ludo.productionsmx@gmail.com>
2017-11-16 15:49:42 -0600

    Adding image placeholders

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -2,12 +2,34 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_colors);
+        setContentView(R.layout.words_list);
+
+        ArrayList<Word> colors = new ArrayList<Word>();
+        colors.add(new Word("Where are you going?", "minto wuksus"));
+        colors.add(new Word("What is your name?", "tinnә oyaase'nә"));
+        colors.add(new Word("My name is...", "oyaaset..."));
+        colors.add(new Word("How are you feeling?","michәksәs?"));
+        colors.add(new Word("I’m feeling good.",  "kuchi achit"));
+        colors.add(new Word("Are you coming?", "әәnәs'aa?"));
+        colors.add(new Word("Yes, I’m coming.", "hәә’ әәnәm"));
+        colors.add(new Word("I’m coming.", "әәnәm"));
+        colors.add(new Word("Let’s go.", "yoowutis"));
+        colors.add(new Word("Come here.", "әnni'nem"));
+
+
+        WordAdapter phrasesAdapter = new WordAdapter(this, colors);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(phrasesAdapter);
     }
 }
--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -2,12 +2,34 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_family);
+        setContentView(R.layout.words_list);
+
+        ArrayList<Word> family = new ArrayList<Word>();
+        family.add(new Word("Where are you going?", "minto wuksus"));
+        family.add(new Word("What is your name?", "tinnә oyaase'nә"));
+        family.add(new Word("My name is...", "oyaaset..."));
+        family.add(new Word("How are you feeling?","michәksәs?"));
+        family.add(new Word("I’m feeling good.",  "kuchi achit"));
+        family.add(new Word("Are you coming?", "әәnәs'aa?"));
+        family.add(new Word("Yes, I’m coming.", "hәә’ әәnәm"));
+        family.add(new Word("I’m coming.", "әәnәm"));
+        family.add(new Word("Let’s go.", "yoowutis"));
+        family.add(new Word("Come here.", "әnni'nem"));
+
+
+        WordAdapter phrasesAdapter = new WordAdapter(this, family);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(phrasesAdapter);
     }
 }
