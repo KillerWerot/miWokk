d71e4eddcf441630933154e44ef31a9a26551cd0
LudoProductions <ludo.productionsmx@gmail.com>
2017-11-17 19:40:26 -0600

    Importing audio files

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -28,12 +28,15 @@ public class WordAdapter extends ArrayAdapter<Word> {
      * @param context        The current context. Used to inflate the layout file.
      * @param words A List of AndroidFlavor objects to display in a list
      */
+
+    //private int mColorResourceAdapter
     public WordAdapter(Activity context, ArrayList<Word> words) {
         // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
         // the second argument is used when the ArrayAdapter is populating a single TextView.
         // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
         // going to use this second argument, so it can be any value. Here, we used 0.
         super(context, 0, words);
+        //mColorResourceAdapter = colorResourceAdapter;
     }
 
     /**
