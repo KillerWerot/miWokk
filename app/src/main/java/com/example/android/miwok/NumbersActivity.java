7f4d039928111954cd54e38936c641859b5b9da9
LudoProductions <ludo.productionsmx@gmail.com>
2017-11-18 00:48:12 -0600

    play icon

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -22,7 +22,6 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.words_list);
 
 
-
         ListView lista = (ListView) findViewById(R.id.list);
         lista.setBackgroundResource(R.color.category_numbers);
 
@@ -50,11 +49,34 @@ public class NumbersActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Word word = numbers.get(position);
+                releaseMediaPlayer();
                 mediaPlayer = MediaPlayer.create(NumbersActivity.this,word.getAudioResource() );
                 mediaPlayer.start();
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        releaseMediaPlayer();
+                    }
+                });
             }
         });
         }
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mediaPlayer = null;
+        }
+    }
 
     }
 
